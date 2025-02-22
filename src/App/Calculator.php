78c388aaa730a695f539ccc61414495e83f6304a
surok <surokbool@gmail.com>
2025-02-22 13:55:07 +0300

    random number generation added

--- src/App/Calculator.php
+++ src/App/Calculator.php
@@ -2,10 +2,23 @@
 
 namespace Surok\ComposerPackage\App;
 
+use Random\Randomizer;
+
 class Calculator
 {
+    private Randomizer $randomizer;
+    public function __construct()
+    {
+        $this->randomizer = new Randomizer();
+    }
+
     public function sum(float $a, float $b): float
     {
         return $a + $b;
+    }
+
+    public function getRandomFloat(float $min, float $max): float
+    {
+        return $this->randomizer->getFloat($min, $max);
     }
 }
